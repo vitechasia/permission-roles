30269e8fd5ec9534d46d1d4de386996a2ea454d2
bimacoding <bimacoding@gmail.com>
2023-01-18 01:40:59 +0700

    permissions adn roles

--- src/Views/roles/index.blade.php
+++ src/Views/roles/index.blade.php
@@ -8,7 +8,7 @@
                 <h6>Semua Data Role</h6>
             </div>
             <div class="page-btn">
-                @role('role-create')
+                @role('roles-create')
                     <a class="btn btn-added" href="{{ route('roles.create') }}"><img src="assets/img/icons/plus.svg" class="me-2" alt="img"> Tambah Baru</a>
                 @endrole
             </div>
@@ -21,11 +21,30 @@
                             <p>{{ $message }}</p>
                         </div>
                     @endif
-                    <div class="float-right mb-1">
-                        <form action="" method="get">
-                            <input type="search" name="keyword" class="form-control" placeholder="Pencarian..."
-                                value="{!! !empty(Request::get('keyword')) ? Request::get('keyword') : '' !!}">
-                        </form>
+                    <div class="table-top">
+                        <div class="search-set">
+                            <div class="search-input">
+                                <a class="btn btn-searchset">
+                                    <img src="{{url('/assets/img/icons/search-white.svg')}}" alt="img">
+                                </a>
+                                <form action="" method="get">
+                                    <label> <input type="search" class="form-control form-control-sm" name="keyword" placeholder="Search..." value="{!! !empty(Request::get('keyword')) ? Request::get('keyword') : '' !!}"></label>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="wordset">
+                            {{-- <ul>
+                                <li>
+                                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="pdf" aria-label="pdf"><img src="assets/img/icons/pdf.svg" alt="img"></a>
+                                </li>
+                                <li>
+                                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="excel" aria-label="excel"><img src="assets/img/icons/excel.svg" alt="img"></a>
+                                </li>
+                                <li>
+                                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="print" aria-label="print"><img src="assets/img/icons/printer.svg" alt="img"></a>
+                                </li>
+                            </ul> --}}
+                        </div>
                     </div>
                     <div class="table-responsive">
                         <table class="table table-striped mb-0">
